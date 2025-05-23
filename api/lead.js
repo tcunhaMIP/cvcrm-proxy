9e442b647ff42f97a9195c4b33bd10e5fe596d08
tcunhaMIP <thiago.cunha@mipconstrutora.com.br>
2025-05-23 09:42:32 -0300

    update CORS (-www)

--- api/lead.js
+++ api/lead.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "https://www.jardins156.com.br");
+  res.setHeader("Access-Control-Allow-Origin", "https://jardins156.com.br");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, token");
 
