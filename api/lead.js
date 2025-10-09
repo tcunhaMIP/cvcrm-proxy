2112ae98c41d72035bf5af77bf4f6809b4cbdb8b
tcunhaMIP <thiago.cunha@mipconstrutora.com.br>
2025-10-09 10:26:16 -0300

    feat: allow janeiro

--- api/lead.js
+++ api/lead.js
@@ -5,6 +5,8 @@ export default async function handler(req, res) {
     "https://www.jardins156.com.br",
     "https://mipconstrutora.com.br",
     "https://www.mipconstrutora.com.br",
+    "https://janeiroengenharia.com.br",
+    "https://www.janeiroengenharia.com.br",
     "http://127.0.0.1:5500", // desenvolvimento local
     "http://localhost:5500", // alternativa comum
   ];
